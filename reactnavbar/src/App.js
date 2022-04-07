0821c3b63beffa2a7e273f9d311de3f6fb84a830
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2022-04-07 22:02:00 +0530

    navbar_with_react

---
+++ reactnavbar/src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const menu = ["Services","Projects","About"];
+  return (
+    <div className="App">
+    <div id = "container">
+      <h1>LOGOBAKERY</h1>
+      <div id = "menu">
+        {
+          menu.map((ele) => {
+             return  <Menudrop menudrop = {ele}/>
+          })
+        }
+       
+      </div>
+      <button>Contacts</button>
+    </div>
+    </div>
+  );
+}
+
+function Menudrop(menus){
+ 
+   return <p id = "title">{menus.menudrop}</p>
+}
+export default App;
