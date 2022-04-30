c8278a3ea17bdf131280d454103c8a48e4d98bc4
Aakash Vishwakarma <97448935+AakaShVSh@users.noreply.github.com>
2022-04-30 22:37:57 +0530

    Add files via upload

---
+++ combine reducer/src/components/authcontext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext()
+
+
+
+export const AuthContextProvider = ({ children }) => {
+    let [auth, login] = useState( {status:false})
+
+
+    
+
+    return <AuthContext.Provider value={{ auth, login }}>{children}</AuthContext.Provider>
+}
